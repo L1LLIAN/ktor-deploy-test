ee8a4d070ddfd9afef227b70a1503741ef0105c4
Lillian Armes <me@lily.lol>
2022-06-15 04:06:49 -0400

    edit index route and add shadow

--- src/main/kotlin/lol/lily/Application.kt
+++ src/main/kotlin/lol/lily/Application.kt
@@ -2,12 +2,17 @@ package lol.lily
 
 import io.ktor.server.engine.*
 import io.ktor.server.netty.*
-import lol.lily.plugins.*
+import lol.lily.plugins.configureRouting
+import lol.lily.plugins.configureSerialization
+
+val START_TIME = System.currentTimeMillis()
 
 fun main() {
-    embeddedServer(Netty, port = 8080, host = "0.0.0.0") {
+    val port = System.getenv("KTOR_PORT")?.toInt() ?: 8080
+    val host = System.getenv("KTOR_HOST") ?: "0.0.0.0"
+
+    embeddedServer(Netty, port = port, host = host) {
         configureRouting()
-        configureTemplating()
         configureSerialization()
     }.start(wait = true)
 }
--- src/main/kotlin/lol/lily/plugins/Routing.kt
+++ src/main/kotlin/lol/lily/plugins/Routing.kt
@@ -1,16 +1,22 @@
 package lol.lily.plugins
 
-import io.ktor.server.routing.*
-import io.ktor.http.*
 import io.ktor.server.application.*
-import io.ktor.server.response.*
-import io.ktor.server.request.*
+import io.ktor.server.html.*
+import io.ktor.server.routing.*
+import kotlinx.html.body
+import kotlinx.html.h1
+import kotlinx.html.p
+import lol.lily.START_TIME
 
 fun Application.configureRouting() {
-
     routing {
         get("/") {
-            call.respondText("Hello World!")
+            call.respondHtml {
+                body {
+                    h1 { +"Ktor Deploy Test" }
+                    p { +"Started at $START_TIME" }
+                }
+            }
         }
     }
 }
